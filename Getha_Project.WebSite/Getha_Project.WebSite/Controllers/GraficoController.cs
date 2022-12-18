9dc0ad6ab4963ddd7122aefd8e02bbf5a4171c90
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-18 00:28:45 -0300

    construção da controller.

---
+++ Getha_Project.WebSite/Getha_Project.WebSite/Controllers/GraficoController.cs
@@ -0,0 +1,32 @@
+﻿using Getha_Project.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Getha_Project.WebSite.Controllers
+{
+    public class GraficoController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public GraficoController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Getha_Project.WebSite/Getha_Project.WebSite/Controllers/HomeController.cs
+++ Getha_Project.WebSite/Getha_Project.WebSite/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Getha_Project.WebSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Configuration;
 
 namespace Getha_Project.WebSite.Controllers
 {
@@ -15,6 +17,7 @@ namespace Getha_Project.WebSite.Controllers
 
         public IActionResult Index()
         {
+            teste();
             return View();
         }
 
@@ -27,6 +30,28 @@ namespace Getha_Project.WebSite.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+        public void teste()
+        {
+
+            var baseAddress = "https://localhost:7232/api/UserLogin?id=1";
+
+            using (var http = new HttpClient())
+            {
+                http.BaseAddress = new Uri(baseAddress);
+                http.DefaultRequestHeaders.Accept.Clear();
+                http.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                http.Timeout = TimeSpan.FromMinutes(30);
+
+                HttpResponseMessage response = http.GetAsync($"{baseAddress}").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string stringContent = response.Content.ReadAsStringAsync().Result;
+
+                }
+            }
         }
     }
 }
