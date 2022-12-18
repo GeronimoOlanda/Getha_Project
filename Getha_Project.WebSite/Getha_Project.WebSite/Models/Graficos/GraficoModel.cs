ff53731a4a8daa6395e84cccd24b7b755ae3bc38
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-18 00:29:18 -0300

    Model para exibir o grafico

---
+++ Getha_Project.WebSite/Getha_Project.WebSite/Models/Graficos/GraficoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Getha_Project.WebSite.Models.Home
+{
+    public class GraficoModel
+    {
+        public int Id { get; set; }
+        public string NomeGrafico { get; set; }
+        public string Descricao { get; set; }
+        public string Valor { get; set; }
+        public string TipoGrafico { get; set; }
+    }
+}
