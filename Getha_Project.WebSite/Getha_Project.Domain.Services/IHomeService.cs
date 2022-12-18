31b27dcfc35b9799a211c8d4c0631fa77fe6cbc1
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-18 11:34:07 -0300

    Construção Interface Service

---
+++ Getha_Project.WebSite/Getha_Project.Domain.Services/IHomeService.cs
@@ -0,0 +1,14 @@
+﻿using Getha_Project.Domain.Entities.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getha_Project.Domain.Services
+{
+    public interface IHomeService
+    {
+        UserLoginDTO consultaUsuarioPorId(int idUsuario);
+    }
+}
