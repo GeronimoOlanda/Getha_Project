18c352cd5736f595c6af2bb146bf892c39050768
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-18 11:34:57 -0300

    Retornando o valor na nossa Controller e adicionando a dependencia na program.

--- Getha_Project.WebSite/Getha_Project.WebSite/Controllers/HomeController.cs
+++ Getha_Project.WebSite/Getha_Project.WebSite/Controllers/HomeController.cs
@@ -3,55 +3,35 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Configuration;
+using Getha_Project.Domain.Services.Impl.DTO;
+using Getha_Project.Domain.Services;
+using Getha_Project.WebSite.Models.Home;
 
 namespace Getha_Project.WebSite.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IHomeService _homeService;
+        public HomeController(ILogger<HomeController> logger, IHomeService homeService)
         {
             _logger = logger;
+            _homeService = homeService;
         }
 
         public IActionResult Index()
         {
-            teste();
-            return View();
+            int idUsuario = 5;
+
+            var resposta = _homeService.consultaUsuarioPorId(idUsuario);
+            return View("Index");
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        
-        public void teste()
-        {
-
-            var baseAddress = "https://localhost:7232/api/UserLogin?id=1";
-
-            using (var http = new HttpClient())
-            {
-                http.BaseAddress = new Uri(baseAddress);
-                http.DefaultRequestHeaders.Accept.Clear();
-                http.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                http.Timeout = TimeSpan.FromMinutes(30);
-
-                HttpResponseMessage response = http.GetAsync($"{baseAddress}").Result;
-                if (response.IsSuccessStatusCode)
-                {
-                    string stringContent = response.Content.ReadAsStringAsync().Result;
-
-                }
-            }
         }
     }
 }
--- Getha_Project.WebSite/Getha_Project.WebSite/Program.cs
+++ Getha_Project.WebSite/Getha_Project.WebSite/Program.cs
@@ -1,7 +1,11 @@
+using Getha_Project.Domain.Services;
+using Getha_Project.Domain.Services.Impl;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IHomeService, HomeService>();
 
 var app = builder.Build();
 
