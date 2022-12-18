e61b595e0376d52a3e79a1268d59e33b3809fa11
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-17 22:09:56 -0300

    criação do projeto website.

---
+++ Getha_Project.WebSite/Getha_Project.WebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Getha_Project.WebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
