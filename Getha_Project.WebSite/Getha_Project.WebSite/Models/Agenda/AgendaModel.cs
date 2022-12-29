702b9afc7a521b8451cec9672f3944b6bc5be417
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-28 23:12:49 -0300

    Adicionando os atributos na nossa AgendaModel.

--- Getha_Project.WebSite/Getha_Project.WebSite/Models/Agenda/AgendaModel.cs
+++ Getha_Project.WebSite/Getha_Project.WebSite/Models/Agenda/AgendaModel.cs
@@ -2,5 +2,11 @@
 {
     public class AgendaModel
     {
+        public int Id_Agenda { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Observacoes { get; set; }
+        public string Detalhes { get; set; }
+        public string flagExibir { get; set; }
     }
 }
