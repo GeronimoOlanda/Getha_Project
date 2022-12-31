2c60487f06d3050c46d582451cda1b57eecdd586
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-30 23:38:48 -0300

    Criando nosso dto de reposta da nossa api.

---
+++ Getha_Project.WebSite/Getha_Project.Domain.Entities/DTO/RespostaAgendaDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getha_Project.Domain.Services.Impl.DTO
+{
+    public class RespostaAgendaDTO
+    {
+        public int Id_Agenda { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Observacoes { get; set; }
+        public string Detalhes { get; set; }
+        public string flagExibir { get; set; }
+    }
+}
